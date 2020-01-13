f01d1f93badfe8c122bc06946498fade60a05559
Eduard Puigdemunt <puigdemunt@gmail.com>
2020-01-13 13:05:45 +0100

    Create StorePaymentModel.php

---
+++ src/Models/StorePaymentModel.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace shoppingbox\core;
+
+use Illuminate\Database\Eloquent\Model;
+
+class StorePaymentModel extends Model
+{
+    
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'store_payments';
+
+    /**
+     * Indicates if the model should be timestamped.
+     *
+     * @var bool
+     */
+    public $timestamps = true;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'orders'
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'created_at', 'updated_at',
+    ];
+}
